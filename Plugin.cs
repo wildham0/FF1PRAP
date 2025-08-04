938aae3ffba70b1bc886606c7d1f281da23e6be0
wildham0 <64616385+wildham0@users.noreply.github.com>
2025-08-04 07:40:53 -0400

    Fix bridge

--- Plugin.cs
+++ Plugin.cs
@@ -29,7 +29,7 @@ namespace FF1PRAP;
 public class PluginInfo
 {
 	public const string NAME = "FF1 Pixel Remaster AP";
-	public const string VERSION = "0.1.9";
+	public const string VERSION = "0.1.10";
 	public const string GUID = "wildham.ff1pr.randomizer";
 }
 
