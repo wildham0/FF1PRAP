51aa1e51e2623176002adf5f3f16c197da77577f
wildham0 <64616385+wildham0@users.noreply.github.com>
2025-08-24 23:46:13 -0400

    Version bump

--- Plugin.cs
+++ Plugin.cs
@@ -32,7 +32,7 @@ namespace FF1PRAP;
 public class PluginInfo
 {
 	public const string NAME = "FF1 Pixel Remaster AP";
-	public const string VERSION = "0.3.0";
+	public const string VERSION = "0.3.1";
 	public const string GUID = "wildham.ff1pr.randomizer";
 }
 
