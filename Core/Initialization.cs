2a5e1336dc083f53b0f5ed2d046552cdaffaf3fe
wildham0 <64616385+wildham0@users.noreply.github.com>
2026-03-19 22:23:50 -0400

    Fix some chest locations order

--- Core/Initialization.cs
+++ Core/Initialization.cs
@@ -216,7 +216,9 @@ namespace FF1PRAP
 			//GameData.OwnedItemsClient.AddOwnedItem((int)Items.Canoe, 1);
 			GameData.OwnedItemsClient.AddOwnedItem((int)Items.RosettaStone, 1);
 			//GameData.OwnedItemsClient.AddOwnedItem((int)Items.JoltTonic, 1);
-			//GameData.OwnedItemsClient.AddOwnedItem((int)Items.Oxyale, 1);
+			GameData.OwnedItemsClient.AddOwnedItem((int)Items.Oxyale, 1);
+			GameData.OwnedItemsClient.AddOwnedItem((int)Items.Chime, 1);
+			GameData.OwnedItemsClient.AddOwnedItem((int)Items.WarpCube, 1);
 			GameData.OwnedItemsClient.AddOwnedItem((int)Items.Levistone, 1);
 			GameData.OwnedItemsClient.AddOwnedItem((int)Items.Lute, 1);
 			GameData.DataStorage.Set(DataStorage.Category.kScenarioFlag1, (int)ScenarioFlags.Canal, 1); // Force visit King in Coneria
